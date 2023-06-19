0893693d1bcd7bc131789e42e8286c1c79f63079
UBC Student <jhan27@pender.students.cs.ubc.ca>
2023-06-19 15:02:25 -0700

    update index

--- index.php
+++ index.php
@@ -5,7 +5,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Index</title>
 </head>
+
 <body>
+  <?php	
+    // load up your config file 
+    require_once("./src/php/resources/config.php");
+    
+    require_once(TEMPLATES_PATH . "/header.php");
+  ?>
   
 </body>
 </html>
