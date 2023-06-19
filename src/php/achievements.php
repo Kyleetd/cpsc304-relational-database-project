e38f01f42264fbaeb822350f3213d667b10a4d22
kyleetd <kyleetd@gmail.com>
2023-06-18 23:57:47 -0700

    Added and "Add" button to the goals page

--- src/php/achievements.php
+++ src/php/achievements.php
@@ -12,6 +12,16 @@
             padding: 10px;
             background-color: #f2f2f2;
         }
+        table {
+        margin: auto;
+        border-collapse: collapse;
+        width: 80%;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
     </style>
 
     <div class="header">
@@ -19,7 +29,39 @@
     </div>
 
     <?php
-    
+    // Establish a connection to the Oracle database
+    $db_conn = OCILogon("ora_kyleetd", "a78242021", "dbhost.students.cs.ubc.ca:1522/stu");
+
+    // Check if the connection was successful
+    if (!$db_conn) {
+        $e = oci_error();
+        trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    }
+
+    // Prepare and execute the SQL query
+    $query = "SELECT * FROM User_Achievement";
+    $stmt = oci_parse($db_conn, $query);
+    oci_execute($stmt);
+
+     // Display the table
+     echo '<table>';
+     echo '<tr><th>Achievement ID</th><th>Description</th><th>Date Accomplished</th><th>User ID</th><th>Goal ID</th></tr>';
+
+     while ($row = oci_fetch_assoc($stmt)) {
+        echo '<tr>';
+        echo '<td>'.$row['ACHIEVEMENTID'].'</td>';
+        echo '<td>'.$row['DESCRIPTION'].'</td>';
+        echo '<td>'.$row['DATEACCOMPLISHED'].'</td>';
+        echo '<td>'.$row['USERID'].'</td>';
+        echo '<td>'.$row['GOALID'].'</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+
+    // Close the database connection
+    oci_free_statement($stmt);
+    oci_close($db_conn);
+
     ?>
 
     </body>
--- src/php/goals.php
+++ src/php/goals.php
@@ -1,157 +1,127 @@
 <html>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Goals</title>
-    </head>
-
-    <body>
-        <h2>Add a Fitness Goal</h2>
-        <form method="POST" action="Goals.php"> <!--refresh page when submitted-->
-            <input type="hidden" id="insertQueryRequest" name="insertQueryRequest">
-            Goal ID: <input type="number" name="goalID"> <br /><br />
-            Description: <input type="text" name="description"> <br /><br />
-            Target Date: <input type="text" name="targetDate"> <br /><br />
-
-            <input type="submit" value="Insert" name="insertSubmit"></p>
-        </form>
-
-        <hr />
-
-        <h2>Mark a Goal as Achieved</h2>
-
-        <form method="POST" action="Goals.php"> <!--refresh page when submitted-->
-            <input type="hidden" id="goalAchievedRequest" name="goalAchievedRequest">
-            Goal ID: <input type="text" name="goalID"> <br /><br />
-            Date Achieved: <input type="text" name="dateAchieved"> <br /><br />
-
-            <input type="submit" value="Mark Achieved" name="goalAchievedSubmit"></p>
-        </form>
-
-        <hr />
-
-        <h2>Display Goals</h2>
-        <form method="GET" action="Goals.php">
-            <input type="hidden" id="displayGoalsRequest" name="displayGoalsRequest">
-            <input type="submit" value="Display Goals" name="displayGoals">
-        </form>
-
-<?php
-
-// Establish a connection to the Oracle database
-$db_conn = OCILogon("ora_kyleetd", "a78242021", "dbhost.students.cs.ubc.ca:1522/stu");
-
-// Check if the connection was successful
-if ($db_conn) {
-    
-    // Drop the "GOALSTABLE" table
-    $query = "DROP TABLE GOALSTABLE";
-    $stmt = oci_parse($db_conn, $query);
-    oci_execute($stmt);
-    
-    // Execute the SQL query to retrieve table names
-    $query = "SELECT table_name FROM user_tables";
-    $stmt = oci_parse($db_conn, $query);
-    oci_execute($stmt);
-
-    // Fetch and display the table names
-    echo "<h1>Tables in the Database:</h1>";
-    while ($row = oci_fetch_array($stmt, OCI_ASSOC)) {
-        echo "<p>{$row['TABLE_NAME']}</p>";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Goals</title>
+</head>
+<style>
+    .header {
+        text-align: center;
+        font-size: 25px;
+        padding: 10px;
+        background-color: #f2f2f2;
     }
 
-    // Free the statement and close the connection
-    oci_free_statement($stmt);
-    oci_close($db_conn);
-} else {
-    // Display an error message if the connection failed
-    echo "<h1>Failed to connect to the database.</h1>";
-}
-
-function executePlainSQL($cmdstr) {
-    global $db_conn, $success;
-
-    $statement = OCIParse($db_conn, $cmdstr);
-    //There are a set of comments at the end of the file that describe some of the OCI specific functions and how they work
-
-    if (!$statement) {
-        echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-        $e = OCI_Error($db_conn); // For OCIParse errors pass the connection handle
-        echo htmlentities($e['message']);
-        $success = False;
+    table {
+        margin: auto;
+        border-collapse: collapse;
+        width: 80%;
     }
 
-    $r = OCIExecute($statement, OCI_DEFAULT);
-    if (!$r) {
-        echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-        $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
-        echo htmlentities($e['message']);
-        $success = False;
+    th,
+    td {
+        padding: 8px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
     }
 
-    return $statement;
-}
-
-function executeBoundSQL($cmdstr, $list) {
-    global $db_conn, $success;
-    $statement = OCIParse($db_conn, $cmdstr);
-
-    if (!$statement) {
-        echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-        $e = OCI_Error($db_conn);
-        echo htmlentities($e['message']);
-        $success = False;
+    .add-goal-button {
+        display: inline-block;
+        width: 30px;
+        height: 30px;
+        line-height: 30px;
+        text-align: center;
+        background-color: #f2f2f2;
+        border: 1px solid #ddd;
+        border-radius: 3px;
+        cursor: pointer;
     }
 
-    foreach ($list as $tuple) {
-        foreach ($tuple as $bind => $val) {
-            OCIBindByName($statement, $bind, $val);
-            unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
-        }
-
-        $r = OCIExecute($statement, OCI_DEFAULT);
-        if (!$r) {
-            echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-            $e = OCI_Error($statement); // For OCIExecute errors, pass the statementhandle
-            echo htmlentities($e['message']);
-            echo "<br>";
-            $success = False;
-        }
+    .hidden-row {
+        display: none;
     }
-}
-
-function printResult($result) { //prints results from a select statement
-    echo "<br>Retrieved data from table User_FitnessGoal:<br>";
-    echo "<table>";
-    echo "<tr><th>goalID</th><th>Description</th><th>targetDate</th></tr>";
-
-    while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-        echo "<tr><td>" . $row["goalID"] . "</td><td>" . $row["Description"] . "</td><td>" . $row["targetDate"] . "</td></tr>";
+</style>
+<script>
+    function showInputForm() {
+        var formRow = document.getElementById('form-row');
+        formRow.style.display = 'table-row';
     }
+</script>
+</head>
+<body>
 
-    echo "</table>";
-}
+    <div class="header">
+        <h1>Fitness Goals</h1>
+        <div class="add-goal-button" onclick="showInputForm()">+</div>
+    </div>
 
-function connectToDB() {
-    global $db_conn;
-
-    // Your username is ora_(CWL_ID) and the password is a(student number). For example,
-    // ora_platypus is the username and a12345678 is the password.
+    <?php
+    // Establish a connection to the Oracle database
     $db_conn = OCILogon("ora_kyleetd", "a78242021", "dbhost.students.cs.ubc.ca:1522/stu");
 
-    if ($db_conn) {
-        debugAlertMessage("Database is Connected");
-        return true;
-    } else {
-        debugAlertMessage("Cannot connect to Database");
-        $e = OCI_Error(); // For OCILogon errors pass no handle
-        echo htmlentities($e['message']);
-        return false;
+    // Check if the connection was successful
+    if (!$db_conn) {
+        $e = oci_error();
+        trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
     }
-}
 
-?>
+    // Prepare and execute the SQL query
+    $query = "SELECT * FROM User_FitnessGoal";
+    $stmt = oci_parse($db_conn, $query);
+    oci_execute($stmt);
 
-	</body>
+    // Display the table
+    echo '<table>';
+    echo '<tr><th>Goal ID</th><th>Description</th><th>Target Date</th><th>User ID</th></tr>';
+
+    while ($row = oci_fetch_assoc($stmt)) {
+        echo '<tr>';
+        echo '<td>' . $row['GOALID'] . '</td>';
+        echo '<td>' . $row['DESCRIPTION'] . '</td>';
+        echo '<td>' . $row['TARGETDATE'] . '</td>';
+        echo '<td>' . $row['USERID'] . '</td>';
+        echo '</tr>';
+    }
+
+    // Display the input form row
+    echo '<tr id="form-row" class="hidden-row">';
+    echo '<td></td>';
+    echo '<td>';
+    echo '<form method="post" action="">';
+    echo '<input type="text" name="description" placeholder="Enter goal description">';
+    echo '</td>';
+    echo '<td><input type="text" name="targetDate" placeholder="Enter target date"></td>';
+    echo '<td>';
+    echo '<input type="submit" name="submit" value="Add">';
+    echo '</form>';
+    echo '</td>';
+    echo '</tr>';
+
+    echo '</table>';
+
+    // Handle form submission
+    if (isset($_POST['submit'])) {
+        $description = $_POST['description'];
+        $targetDate = $_POST['targetDate'];
+
+        // Perform the database insertion
+        $insertQuery = "INSERT INTO User_FitnessGoal (DESCRIPTION, TARGETDATE) VALUES (:description, :targetDate)";
+        $insertStmt = oci_parse($db_conn, $insertQuery);
+        oci_bind_by_name($insertStmt, ":description", $description);
+        oci_bind_by_name($insertStmt, ":targetDate", $targetDate);
+        oci_execute($insertStmt);
+
+        // Refresh the page to display the updated table
+        header("Refresh:0");
+    }
+
+    // Close the database connection
+    oci_free_statement($stmt);
+    oci_close($db_conn);
+
+    ?>
+
+</body>
+
 </html>
