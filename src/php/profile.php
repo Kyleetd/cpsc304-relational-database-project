68aa5c1dd19689c1f0b31492e38c8df9b3e5ef54
gargkash <gargkash@students.cs.ubc.ca>
2023-06-21 15:13:57 -0700

    Updated profile page queries

--- src/php/profile.php
+++ src/php/profile.php
@@ -88,10 +88,12 @@
 </div>
 
 <div id="filter-line">
-    COUNT USERS HAVING BMI
-    >
-    <input type="text" id="filter-input" placeholder="Enter BMI value">
-    <button onclick="applyFilter()">Find Count</button>
+    <form method="post">
+        COUNT USERS HAVING BMI
+        >
+        <input type="text" id="filter-input" name="filter-input" placeholder="Enter BMI value">
+        <button type="submit" name="apply_filter">Find Count</button>
+    </form>
 </div>
 
 <?php
@@ -105,82 +107,132 @@ if (!$db_conn) {
 }
 
 // define & execute the SQL query
-$query = "SELECT Gym.address, Gym.postalCode, PCC.country, Gym.city, Gym.name, Attends.userID
-          FROM Gym
-          LEFT JOIN Attends ON Gym.address = Attends.address AND Gym.postalCode = Attends.postalCode
-          LEFT JOIN PCC ON Gym.postalCode = PCC.postalCode";
+$query = "SELECT User_Measurement.userID, User.name, User_Measurement.height, User_Measurement.weight, User_Measurement.BMI
+          FROM User_Measurement, User
+          WHERE User_Measurement.userID = User.ID";
 $stmt = oci_parse($db_conn, $query);
 oci_execute($stmt);
 
 // Display the table
+echo '<form method="post" action="">';
 echo '<table>';
 echo '<tr><th>UserID</th><th>Name</th><th>Height</th><th>Weight</th><th>BMI</th><th></th></tr>';
 
-echo '<form method="post" action="">';
-
 while ($row = oci_fetch_assoc($stmt)) {
     echo '<tr>';
-    echo '<td data-column="address">'.$row['ADDRESS'].'</td>';
-    echo '<td data-column="postalCode">'.$row['POSTALCODE'].'</td>';
-    echo '<td data-column="city">'.$row['CITY'].'</td>';
-    echo '<td data-column="name">'.$row['NAME'].'</td>';
-    echo '<td data-column="country">'.$row['COUNTRY'].'</td>';
     echo '<td data-column="userID">'.$row['USERID'].'</td>';
+    echo '<td data-column="name">'.$row['NAME'].'</td>';
+    echo '<td data-column="height">'.$row['HEIGHT'].'</td>';
+    echo '<td data-column="weight">'.$row['WEIGHT'].'</td>';
+    echo '<td data-column="BMI">'.$row['BMI'].'</td>';
     echo '</tr>';
 }
 
 // Display input form row (last row) if '+' button is clicked
 echo '<tr id="form-row" class="hidden-row">';
-echo '<td><input type="text" name="userID" placeholder="Enter UserID"></td>';
-echo '<td><input type="text" name="name" placeholder="Enter Name"></td>';
-echo '<td><input type="text" name="height" placeholder="Enter Height"></td>';
-echo '<td><input type="text" name="weight" placeholder="Enter weight"></td>';
-echo '<td><input type="text" name="BMI" placeholder="Enter BMI"></td>';
+echo '<td><input type="number" name="userID" placeholder="Enter user ID"></td>';
+echo '<td><input type="text" name="name" placeholder="Enter name"></td>';
+echo '<td><input type="number" name="height" placeholder="Enter height"></td>';
+echo '<td><input type="number" name="weight" placeholder="Enter weight"></td>';
+echo '<td><input type="number" name="BMI" placeholder="Enter BMI"></td>';
 echo '<td colspan="2">';
 echo '<input type="submit" name="submit" value="Add">';
 echo '</td>';
 echo '</tr>';
-
 echo '</table>';
-
 echo '</form>'; 
 
 // Handle form submission
 if (isset($_POST['submit'])) {
-    $address = $_POST['address'];
-    $postalCode = $_POST['postalCode'];
-    $city = $_POST['city'];
+    $userID = $_POST['userID'];
     $name = $_POST['name'];
-    $country = $_POST['country'];
-    $userID = (int) $_POST['userID'];
+    $height = $_POST['height'];
+    $weight = $_POST['weight'];
+    $BMI = $_POST['BMI'];
 
-    // Insert Gym in Gym table
-    $insertQuery = "INSERT INTO Gym (ADDRESS, POSTALCODE, CITY, NAME) VALUES (:address, :postalCode, :city, :name)";
+    // Insert User in User_Measurement table
+    $insertQuery = "INSERT INTO User_Measurement (HEIGHT, WEIGHT, BMI, USERID) VALUES (:height, :weight, :BMI, :userID)";
     $insertStmt = oci_parse($db_conn, $insertQuery);
-    oci_bind_by_name($insertStmt, ":address", $address);
-    oci_bind_by_name($insertStmt, ":postalCode", $postalCode);
-    oci_bind_by_name($insertStmt, ":city", $city);
-    oci_bind_by_name($insertStmt, ":name", $name);
-    oci_execute($insertStmt);
-
-    // Insert Gym in PCC table
-    $insertQuery = "INSERT INTO PCC (POSTALCODE, COUNTRY) VALUES (:postalCode, :country)";
-    $insertStmt = oci_parse($db_conn, $insertQuery);
-    oci_bind_by_name($insertStmt, ":postalCode", $postalCode);
-    oci_bind_by_name($insertStmt, ":country", $country);
-    oci_execute($insertStmt);
-
-    // Insert Gym in Attends table
-    $insertQuery = "INSERT INTO Attends (ADDRESS, POSTALCODE, USERID) VALUES (:address, :postalCode, :userID)";
-    $insertStmt = oci_parse($db_conn, $insertQuery);
-    oci_bind_by_name($insertStmt, ":address", $address);
-    oci_bind_by_name($insertStmt, ":postalCode", $postalCode);
+    oci_bind_by_name($insertStmt, ":height", $height);
+    oci_bind_by_name($insertStmt, ":weight", $weight);
+    oci_bind_by_name($insertStmt, ":BMI", $BMI);
     oci_bind_by_name($insertStmt, ":userID", $userID);
     oci_execute($insertStmt);
 
+    // Insert User in User table
+    $insertQuery = "INSERT INTO User (ID, NAME) VALUES (:ID, :name)";
+    $insertStmt = oci_parse($db_conn, $insertQuery);
+    oci_bind_by_name($insertStmt, ":ID", $userID);
+    oci_bind_by_name($insertStmt, ":name", $name);
+    oci_execute($insertStmt);
+
     // Refresh table
-    header("Refresh:0");
-} 
+    echo '<script>';
+    echo 'document.addEventListener("DOMContentLoaded", function() {';
+    echo '    var formRow = document.getElementById("form-row");';
+    echo '    formRow.style.display = "none";';
+    echo '    var tableBody = document.querySelector("table tbody");';
+    echo '    tableBody.innerHTML = `' . $tableRows . '`;';
+    echo '});';
+    echo '</script>';
+} else if (isset($_POST['apply_filter'])) {
+
+    // Get the filter input value
+    $filterDropdown = $_POST['filter-dropdown'];
+    $filterValue = trim(strtolower($_POST['filter-input']));
+
+    // Create a view for the joinedAll table
+    $viewQuery = "CREATE VIEW joinedAll AS
+                SELECT Gym.address, Gym.postalCode, PCC.country, Gym.city, Gym.name, Attends.userID
+                FROM Gym
+                LEFT JOIN Attends ON Gym.address = Attends.address AND Gym.postalCode = Attends.postalCode
+                LEFT JOIN PCC ON Gym.postalCode = PCC.postalCode";
+
+    // Execute the view creation query
+    $createViewStmt = oci_parse($db_conn, $viewQuery);
+    oci_execute($createViewStmt);
+
+    // Perform a separate query on the view
+    $filterQuery = "SELECT address, postalCode, country, city, name, userID
+                    FROM joinedAll
+                    WHERE LOWER(" . $filterDropdown . ") = :filterValue";
+
+    $filterStmt = oci_parse($db_conn, $filterQuery);
+    oci_bind_by_name($filterStmt, ":filterValue", $filterValue);
+    oci_bind_by_name($filterStmt, ":filterDropdown", $filterDropdown);
+    oci_execute($filterStmt);    
+
+    // Fetch all rows from the executed statement into an array
+    $rows = oci_fetch_all($stmt, $result, null, null, OCI_FETCHSTATEMENT_BY_ROW);
+
+    // Display the filtered table
+    echo '<form method="post" action="">';
+    echo '<table>';
+    echo '<tr><th>Address</th><th>Postal Code</th><th>City</th><th>Name</th><th>Country</th><th>UserID</th></tr>';
+
+    while ($row = oci_fetch_assoc($filterStmt)) {
+        echo '<tr>';
+        echo '<td data-column="address">' . $row['ADDRESS'] . '</td>';
+        echo '<td data-column="postalCode">' . $row['POSTALCODE'] . '</td>';
+        echo '<td data-column="city">' . $row['CITY'] . '</td>';
+        echo '<td data-column="name">' . $row['NAME'] . '</td>';
+        echo '<td data-column="country">' . $row['COUNTRY'] . '</td>';
+        echo '<td data-column="userID">' . $row['USERID'] . '</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+    echo '</form>';
+
+    // Refresh table
+    echo '<script>';
+    echo 'document.addEventListener("DOMContentLoaded", function() {';
+    echo '    var formRow = document.getElementById("form-row");';
+    echo '    formRow.style.display = "none";';
+    echo '    var tableBody = document.querySelector("table tbody");';
+    echo '    tableBody.innerHTML = `' . $tableRows . '`;';
+    echo '});';
+    echo '</script>';
+}
 		
 // Close the database connection	
 oci_free_statement($stmt);	
@@ -193,40 +245,8 @@ oci_close($db_conn);
         var formRow = document.getElementById('form-row');
         formRow.style.display = 'table-row';
     }
-    function applyFilter() {
-        var filterDropdown = document.getElementById("filter-dropdown");
-        var filterInput = document.getElementById("filter-input");
-
-        var filterColumn = filterDropdown.value;
-        var filterValue = filterInput.value.trim().toLowerCase();
-
-        var table = document.getElementsByTagName("table")[0];
-        var rows = table.getElementsByTagName("tr");
-
-        for (var i = 1; i < rows.length; i++) {
-            var cells = rows[i].getElementsByTagName("td");
-
-            var rowVisible = false;
-            for (var j = 0; j < cells.length; j++) {
-                var cell = cells[j];
-                var cellValue = cell.innerHTML.trim().toLowerCase();
-
-                if (j === 0 && cellValue === filterValue) {
-                    rowVisible = true;
-                    break;
-                }
-
-                if (j > 0 && filterColumn === cell.getAttribute("data-column") && cellValue === filterValue) {
-                    rowVisible = true;
-                    break;
-                }
-            }
-
-            rows[i].style.display = rowVisible ? "" : "none";
-        }
-    }
-    function openAverageBMI() {
-        window.open("https://www.students.cs.ubc.ca/~gargkash/project_j4i5v_j7r8j_r6z9i/src/php/averageBMI.php", "_blank");
+    function openNumberOfGymsPerCountry() {
+        window.open("https://www.students.cs.ubc.ca/~kyleetd/project_j4i5v_j7r8j_r6z9i/src/php/numberOfGymsPerCountry.php", "_blank");
     }
 </script>
 
