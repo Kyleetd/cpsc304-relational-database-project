d8b2ea53da57ab6f6f0bdab85c20d97348fc7714
UBC Student <jhan27@pender.students.cs.ubc.ca>
2023-06-20 21:38:28 -0700

    selectTable properly displays table

--- src/php/selectTable.php
+++ src/php/selectTable.php
@@ -33,7 +33,7 @@
             //Add columns to columns array
             global $columns;
             $numCols = oci_num_fields($result);
-            for ($i = 1; $i < $numCols; $i++) {
+            for ($i = 1; $i <= $numCols; $i++) {
                 $column = oci_field_name($result, $i);
                 $dataType = oci_field_type($result, $i);
                 $columns[$column] = $dataType;
@@ -42,37 +42,44 @@
         }
     }
 
+    $results = array();
+
     function handleColAndFilterRequest() {
         if (connectToDB()) {
-            $selectedColumns = $_POST['selected_columns'];
-            $filterStatements = $_POST['filter'];
+            global $results;
+            $selectedColumns = $_POST['selected_columns_list'];
+            $filterStatements = $_POST['filter_list'];  
+            $selectedTable = $_POST['table_selection'];
         
             // Build the SELECT statement
-            $selectStatement = "SELECT " . implode(", ", $selectedColumns) . " FROM $selectedTable WHERE ";
+            $selectStatement = "SELECT " . implode(", ", $selectedColumns) . " FROM $selectedTable";
         
             // Build the filter conditions
-            $filterConditions = array();
-            foreach ($filterStatements as $column => $value) {
-                if (!empty($value)) {
-                    $filterConditions[] = "$column = '$value'";
-                }
-            }
-            $filterClause = implode(" AND ", $filterConditions);
+            // $filterConditions = array();
+            // foreach ($filterStatements as $column => $value) {
+            //     if (!empty($value)) {
+            //         $filterConditions[] = "$column = '$value'";
+            //     }
+            // }
+            // $filterClause = implode(" AND ", $filterConditions);
         
             // Finalize the query
-            $query = $selectStatement . $filterClause;
+            $query = $selectStatement; //. $filterClause;
         
             // Execute the query
-            $stmt = oci_parse($conn, $query);
-            oci_execute($stmt);
+            // $stmt = oci_parse($conn, $query);
+            // oci_execute($stmt);
+            $results = executePlainSQL($query);
             disconnectFromDB();
         }
     }
     
-    if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['table_selection'])) {
-        handleTableSelectedRequest();
-    } else if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['selected_columns']) && isset($_POST['filter'])) {
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' 
+        && isset($_POST['selected_columns_list']) && isset($_POST['filter_list'])) {
         handleColAndFilterRequest();
+    } else if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['table_selection'])) {
+        handleTableSelectedRequest();
     }
     ?>
 
@@ -85,8 +92,8 @@
             if (connectToDB()) {
                 $result = executePlainSQL("SELECT table_name FROM user_tables");
 
-                while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-                    echo "<option value=".$row["TABLE_NAME"].">".$row["TABLE_NAME"]."</option>";
+                while ($row = oci_fetch_array($result, OCI_ASSOC)) {
+                    echo "<option value='".$row["TABLE_NAME"]."'>".$row["TABLE_NAME"]."</option>";
                 }
                 disconnectFromDB();
             }
@@ -102,41 +109,46 @@
         <form id="ColumnSelectorForm" name="ColumnSelectorForm" method="post" action="">
             Select all columns you would like to show:
             <?php foreach ($columns as $column => $dataType) : ?>
-                <input type="checkbox" name="selected_columns[]" value="<?php echo $column; ?>">
+                <input type="checkbox" name="selected_columns_list[]" value="<?php echo $column; ?>">
                 <label><?php echo $column; ?></label><br>
                 <?php if ($dataType === 'NUMBER') : ?>
-                    <input type="text" name="filter[<?php echo $column; ?>]" placeholder="integer"><br>
+                    <input type="text" name="filter_list[<?php echo $column; ?>]" placeholder="integer"><br>
                 <?php elseif ($dataType === 'VARCHAR') : ?>
-                    <input type="text" name="filter[<?php echo $column; ?>]" placeholder="text"><br>
+                    <input type="text" name="filter_list[<?php echo $column; ?>]" placeholder="text"><br>
                 <?php elseif ($dataType === 'REAL') : ?>
-                    <input type="text" name="filter[<?php echo $column; ?>]" placeholder="number"><br>
+                    <input type="text" name="filter_list[<?php echo $column; ?>]" placeholder="number"><br>
                 <?php else : ?>
-                    <input type="text" name="filter[<?php echo $column; ?>]" placeholder="text"><br>
+                    <input type="text" name="filter_list[<?php echo $column; ?>]" placeholder="text"><br>
                 <?php endif; ?>
             <?php endforeach; ?>
+
+            <input type="hidden" name="table_selection" value="<?php echo $_POST['table_selection']; ?>">
             <input type="submit" name="Submit" value="Get Table">
         </form>
+    <?php } ?>
 
-        <?php if (isset($stmt)) : ?>
-            <table>
-                <thead>
+    <?php if ($_SERVER['REQUEST_METHOD'] === 'POST' 
+        && isset($_POST['selected_columns_list']) && isset($_POST['filter_list'])) : 
+
+        ?>
+        <table>
+            <thead>
+                <tr>
+                    <?php foreach ($_POST['selected_columns_list'] as $column) : ?>
+                        <th><?php echo $column; ?></th>
+                    <?php endforeach; ?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while ($row = oci_fetch_array($results, OCI_ASSOC)) : ?>
                     <tr>
-                        <?php foreach ($selectedColumns as $column) : ?>
-                            <th><?php echo $column; ?></th>
+                        <?php foreach ($_POST['selected_columns_list'] as $column) : ?>
+                            <td><?php echo $row[$column]; ?></td>
                         <?php endforeach; ?>
                     </tr>
-                </thead>
-                <tbody>
-                    <?php while ($row = oci_fetch_array($stmt, OCI_ASSOC)) : ?>
-                        <tr>
-                            <?php foreach ($selectedColumns as $column) : ?>
-                                <td><?php echo $row[$column]; ?></td>
-                            <?php endforeach; ?>
-                        </tr>
-                    <?php endwhile; ?>
-                </tbody>
-            </table>
-        <?php endif; ?>
-    <?php } ?>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
+    <?php endif; ?>
 </body>
 </html>
