0b317a3ecef2741900ba889f53e1bffe9754a70a
gargkash <gargkash@students.cs.ubc.ca>
2023-06-22 14:26:33 -0700

    Added styling to averageBMI page

--- src/php/averageBMI.php
+++ src/php/averageBMI.php
@@ -7,33 +7,47 @@
 </head>
 
 <style>
+    body {
+        background-image: url('https://i.pinimg.com/564x/a9/80/22/a98022cdb8b339e11542132b6428ac92.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
     .header {
         text-align: center;
         font-size: 25px;
         padding: 10px;
-        background-color: #f2f2f2;
+        background-color: transparent;
+        color: orange; 
+        text-shadow: 2px 2px 4px #5D3FD3;
+    }
+    .button-container {
+    display: inline-block;
+    vertical-align: middle;
     }
     table {
         margin: auto;
         border-collapse: collapse;
         width: 80%;
-        }
+        background-color: #BF40BF; 
+    }
     th, td {
         padding: 8px;
         text-align: left;
-        border-bottom: 1px solid #ddd;
+        border-bottom: 1px solid orange;
+        color: orange; 
     }
     .back-button {
-            position: absolute;
-            top: 10px;
-            left: 10px;
-            padding: 1px 3px;
-            background-color: #f2f2f2;
-            border: 1px solid #ddd;
-            border-radius: 3px;
-            text-decoration: none;
-            color: #333;
-            font-size: 20px;
+        display: inline-block;
+        width: auto; 
+        height: 30px;
+        line-height: 30px;
+        text-align: center;
+        background-color: #f2f2f2;
+        border: 2px solid orange;
+        border-radius: 5px;
+        cursor: pointer;
+        background-color: #BF40BF;
+        color: orange; 
     }
 </style>
 
@@ -64,28 +78,36 @@ $tempQuery = "CREATE VIEW TEMP(userID, average) AS
 $createTempViewStmt = oci_parse($db_conn, $tempQuery);
 oci_execute($createTempViewStmt);
 
-// define & execute the SQL query
-$query = "SELECT userID, average
-          FROM Temp
-          WHERE average < (SELECT AVG(Temp.average) FROM TEMP)";
+// Define the SQL query with the JOIN to Users table
+$query = "SELECT t.userID, u.name, t.average
+          FROM Temp t
+          JOIN Users u ON t.userID = u.ID
+          WHERE t.average < (SELECT AVG(Temp.average) FROM TEMP)";
+
 $stmt = oci_parse($db_conn, $query);
 oci_execute($stmt);
 
 // Display the table
 echo '<table>';
-echo '<tr><th>UserID</th><th>Name</th><th>Height</th><th>Weight</th><th>BMI</th><th></th></tr>';
+echo '<tr><th>UserID</th><th>Name</th><th>Average</th>';
 
 while ($row = oci_fetch_assoc($stmt)) {
     echo '<tr>';
     echo '<td data-column="userID">'.$row['USERID'].'</td>';
     echo '<td data-column="name">'.$row['NAME'].'</td>';
-    echo '<td data-column="height">'.$row['HEIGHT'].'</td>';
-    echo '<td data-column="weight">'.$row['WEIGHT'].'</td>';
-    echo '<td data-column="BMI">'.$row['BMI'].'</td>';
+    echo '<td data-column="average">'.$row['AVERAGE'].'</td>';
     echo '</tr>';
 }
 
 echo '</table>';
+
+// Calculate and display the overall average of BMIs
+$overallAverageQuery = "SELECT AVG(average) AS overall_average FROM TEMP";
+$overallAverageStmt = oci_parse($db_conn, $overallAverageQuery);
+oci_execute($overallAverageStmt);
+$overallAverageRow = oci_fetch_assoc($overallAverageStmt);
+
+echo '<p style="color: cyan;">Overall Average of BMIs: ' . $overallAverageRow['OVERALL_AVERAGE'] . '</p>';
 
 // Close the database connection
 oci_free_statement($stmt);
