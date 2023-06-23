6ec56e5e325f2bbdd3343e75cfe8cda50adda24d
UBC Student <jhan27@pender.students.cs.ubc.ca>
2023-06-22 17:24:59 -0700

    Normalize back button

--- src/php/dashboard.php
+++ src/php/dashboard.php
@@ -4,9 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="../css/dashboard.css" />
-  <style>
-
-  </style>
   <title>Dashboard</title>
 </head>
 <body>
--- src/php/goalsAndAchievements.php
+++ src/php/goalsAndAchievements.php
@@ -5,45 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../css/goalsAndAchievements.css" />
     <title>Goals and Achievements</title>
+    <a href="./dashboard.php" class="back-button">Back</a>
 </head>
 
-<style>
-    body {
-        background-image: url("https://i.pinimg.com/564x/8d/a4/8a/8da48a450c50691b02c50467c20e0434.jpg");
-        background-size: cover;
-        background-repeat: no-repeat;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 100vh;
-        color: orange;
-    }
-    #back-button,
-    #fitnessGoalsButton,
-    #fitnessAchievementsButton {
-        padding: 1px 3px;
-        background-color: orange;
-        border: 1px solid purple;
-        border-radius: 3px;
-        text-decoration: none;
-        color: #7F00FF;
-        font-size: 40px;
-        margin-bottom: 10px;
-        border: 3px solid #8D6CE9; 
-    }
-</style>
-
 <body>
-    <a href="./goals.php">
-        <button id="fitnessGoalsButton">Fitness Goals</button>
-    </a>
-    <a href="./achievements.php">
-        <button id="fitnessAchievementsButton">Fitness Achievements</button>
-    </a>
-    <a href="./dashboard.php">
-        <button id="back-button">Dashboard</button>
-    </a>
+    <div class='button-wrapper'>
+        <a href="./goals.php">
+            <button class='button'>Fitness Goals</button>
+        </a>
+        <a href="./achievements.php">
+            <button class='button'>Fitness Achievements</button>
+        </a>
+    </div>
 
     <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
