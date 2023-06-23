255e266465d96e2e5a3963ea34b2f4b0d9a8e887
UBC Student <jhan27@pender.students.cs.ubc.ca>
2023-06-22 18:43:16 -0700

    Fix styling issues

--- src/php/achievements.php
+++ src/php/achievements.php
@@ -4,48 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Achievements</title>
-    <style>
-    body {
-        background-image: url('https://i.pinimg.com/564x/01/03/21/010321446051fb8a97b258436c7f0e8f.jpg');
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center;
-    }
-    .header {
-        text-align: center;
-        font-size: 25px;
-        padding: 10px;
-        text-shadow: 2px 2px 4px #5D3FD3;
-    }
-    .header h1 {
-        color: #FFB74D; 
-    }
-    table {
-        margin: auto;
-        border-collapse: collapse;
-        width: 80%;
-        background-color: #8D6CE9; 
-    }
-    th, td {
-        padding: 8px;
-        text-align: left;
-        border-bottom: 1px solid orange;
-        color: #FFB74D; 
-    }
-    .back-button {
-        position: absolute;
-        top: 10px;
-        left: 10px;
-        padding: 1px 3px;
-        background-color: #FFB74D; 
-        border: 1px solid #8D6CE9; 
-        border-radius: 3px;
-        text-decoration: none;
-        color: #8D6CE9; 
-        font-size: 20px;
-        text-shadow: none;
-    }
-    </style>
+    <link rel="stylesheet" type="text/css" href="../css/achievements.css">
 </head>
 
 <body>
